d66106254dd8bdd76b716c6eeee7f775932603db
hwihwi99 <chamgrace@naver.com>
2022-07-26 10:37:53 +0900

    first commit

---
+++ src/main/kotlin/com/example/cakezip/CakezipApplication.kt
@@ -0,0 +1,11 @@
+package com.example.cakezip
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CakezipApplication
+
+fun main(args: Array<String>) {
+    runApplication<CakezipApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/cakezip/CakezipApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.cakezip
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CakezipApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
