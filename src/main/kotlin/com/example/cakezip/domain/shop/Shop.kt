763f5202d2ff071d83b0f7ba6643340a28b7d3a6
benny1020 <benny1020@naver.com>
2022-08-01 11:21:58 +0900

    feat: Shop, ShopImg Entity 생성

---
+++ src/main/kotlin/com/example/cakezip/domain/shop/Shop.kt
@@ -0,0 +1,55 @@
+package com.example.cakezip.domain.shop
+
+import com.example.cakezip.domain.BaseEntity
+import lombok.*
+import org.jetbrains.annotations.NotNull
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+class Shop : BaseEntity() {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "shop_id")
+    @NotNull
+    private val shopId: Long = 0
+
+    @Column(name = "shop_name")
+    @NotNull
+    private val shop_name: String = ""
+
+    @Column(name = "business_num")
+    @NotNull
+    private val business_num: String = ""
+
+    @Column(name = "shop_phone_num")
+    @NotNull
+    private val shop_phone_num: String = ""
+
+    @Column(name = "shop_email")
+    @NotNull
+    private val shop_email: String = ""
+
+    @Column(name = "shop_address")
+    @NotNull
+    private val shop_address: String = ""
+
+    @Column(name = "shop_img_description_url")
+    @NotNull
+    private val shop_img_description_url: String = ""
+
+    @Column(name = "shop_short_descriptor")
+    @NotNull
+    private val shop_short_descriptor: String = ""
+
+    private val status: String?=null
+
+}
