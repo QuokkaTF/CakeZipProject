3a8230653eaae960406e3cac3bceab141b7c575d
hyerimmy <heyrim2010@naver.com>
2022-08-08 20:39:11 +0900

    chore:jpa 관련 플러그인 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
     id("io.spring.dependency-management") version "1.0.12.RELEASE"
     kotlin("jvm") version "1.6.21"
     kotlin("plugin.spring") version "1.6.21"
+    kotlin("plugin.jpa") version "1.3.72"
 }
 
 group = "com.example"
