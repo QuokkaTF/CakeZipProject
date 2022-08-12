50ad4bdee3d46f673ab52d3fd7e7efcf70739b8c
hwihwi99 <chamgrace@naver.com>
2022-08-12 21:21:33 +0900

    feat : 클라이언트에게 받은 ajax 정보 받아서 uploadservice와 연결하는 컨트롤러 생성

--- src/main/kotlin/com/example/cakezip/controller/ShopController.kt
+++ src/main/kotlin/com/example/cakezip/controller/ShopController.kt
@@ -2,21 +2,40 @@ package com.example.cakezip.controller
 
 import com.example.cakezip.dto.NewShopReqDto
 import com.example.cakezip.service.ShopService
+import com.example.cakezip.service.UploadStoreImgService
+
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.*
+import org.springframework.web.multipart.MultipartFile
+
+
 
 @Controller
-class ShopController (private val shopService: ShopService){
+class ShopController (private val shopService: ShopService, private val uploadStoreImgService: UploadStoreImgService){
     @GetMapping("/add/shop")
     fun addShop(model: Model):String {
         model.addAttribute("form",NewShopReqDto())
         return "addshop"
     }
 
+    @ResponseBody
+    @RequestMapping(value = arrayOf("/image"), method = arrayOf(RequestMethod.POST))
+    fun imageShop(@RequestParam images: List<MultipartFile>) : ArrayList<String>{
+        var storeUrlList : ArrayList<String> = ArrayList()
+
+        var token:String = uploadStoreImgService.getCloudAPI() // 클라우드 api를 사용하기 위한 곳
+
+        for(image:MultipartFile in images) {
+            var url:String = uploadStoreImgService.upload(image, image.originalFilename.toString(), token)
+            storeUrlList.add(url)
+        }
+        return storeUrlList
+    }
 
     @RequestMapping(value = arrayOf("/add/shop"), method = arrayOf(RequestMethod.POST))
     fun postShop(newShopReqDto: NewShopReqDto) : String{
+        println(newShopReqDto)
         shopService.addNewShop(newShopReqDto)
         return "index"
     }
