fcefd1c2eb2a58ebc2a54ced86429d91076cb13d
hwihwi99 <chamgrace@naver.com>
2022-08-13 16:53:30 +0900

    feat : 판매자 마이페이지 (내 가게, 내 주문 조회) 구현 중

---
+++ src/main/kotlin/com/example/cakezip/controller/SellerController.kt
@@ -0,0 +1,40 @@
+package com.example.cakezip.controller;
+
+import com.example.cakezip.domain.member.Seller
+import com.example.cakezip.domain.shop.Shop
+import com.example.cakezip.repository.ShopRepository
+import com.example.cakezip.service.SellerService
+import com.example.cakezip.service.ShopService
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable
+
+@Controller
+class SellerController (private val sellerService: SellerService, private val shopService: ShopService){
+
+    @GetMapping("/seller/myshop/{sellerId}")
+    fun sellerMyShop(@PathVariable("sellerId") sellerId:Long, model:Model) : String {
+
+        var seller: Seller = sellerService.findBySellerBySellerId(sellerId)
+        var shop : Shop? = shopService.getMyShop(seller)
+        model.addAttribute("shop", shop)
+        if (shop != null) {
+
+        }
+        return "sellermain"
+    }
+
+    @GetMapping("/seller/myorders/{sellerId}")
+    fun sellerMyOrder(@PathVariable("sellerId") sellerId:Long, model:Model) : String {
+        //TODO : 주문 받으면 여기서 조회해야 함. -> 주문 정보 받은 후 여기서 설정하기
+        var seller: Seller = sellerService.findBySellerBySellerId(sellerId)
+        var shop : Shop? = shopService.getMyShop(seller)
+        println(shop)
+        model.addAttribute("shop", shop)
+        return "sellermain"
+    }
+
+
+
+}
