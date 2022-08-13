9c31efe2c71a3d88ed320940788bb0be04f1f80a
hwihwi99 <chamgrace@naver.com>
2022-08-13 16:52:38 +0900

    feat : shop에 대한 imageList 받아오는 repository

--- src/main/kotlin/com/example/cakezip/repository/ShopImgRepository.kt
+++ src/main/kotlin/com/example/cakezip/repository/ShopImgRepository.kt
@@ -1,7 +1,9 @@
 package com.example.cakezip.repository
 
+import com.example.cakezip.domain.shop.Shop
 import com.example.cakezip.domain.shop.ShopImg
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface ShopImgRepository : JpaRepository<ShopImg, Long> {
+    fun findByShop(shop:Shop) : List<ShopImg>
 }
