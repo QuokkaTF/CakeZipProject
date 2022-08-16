718c5e2627a912f41b4d9ebeda1b564273deac4a
benny1020 <benny1020@naver.com>
2022-08-16 16:21:11 +0900

    FIX: build gradle 수정

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,12 +6,13 @@ plugins {
     kotlin("jvm") version "1.6.21"
     kotlin("plugin.spring") version "1.6.21"
     kotlin("plugin.jpa") version "1.3.72"
+    kotlin("kapt") version "1.3.61"
 }
 
 group = "com.example"
 version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_11
-
+val qeurydslVersion = "4.4.0"
 configurations {
     compileOnly {
         extendsFrom(configurations.annotationProcessor.get())
@@ -31,15 +32,24 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("mysql:mysql-connector-java")
+    implementation("org.mybatis.spring.boot:mybatis-spring-boot-starter:2.2.2")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation ("org.webjars:bootstrap:4.5.0")
-    implementation ("org.webjars:jquery:3.5.1")
+    implementation ("org.webjars:bootstrap:5.1.3")
+    implementation ("org.webjars:jquery:3.6.0")
     implementation("org.springframework.boot:spring-boot-starter-security")
     compileOnly("org.projectlombok:lombok")
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
     annotationProcessor("org.projectlombok:lombok")
+    implementation("com.querydsl:querydsl-jpa:$qeurydslVersion")
+    kapt("com.querydsl:querydsl-apt:$qeurydslVersion:jpa")
+    kapt("org.springframework.boot:spring-boot-configuration-processor")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+
     developmentOnly ("org.springframework.boot:spring-boot-devtools")
+}
+
+sourceSets["main"].withConvention(org.jetbrains.kotlin.gradle.plugin.KotlinSourceSet::class) {
+    kotlin.srcDir("$buildDir/generated/source/kapt/main")
 }
 
 tasks.withType<KotlinCompile> {
