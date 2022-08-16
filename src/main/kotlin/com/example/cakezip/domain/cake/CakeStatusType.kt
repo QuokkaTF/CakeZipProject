49fe4df9d4b804b65c8f5c24aa85d45e9930cc65
가영 <kykim097@gmail.com>
2022-08-16 11:12:59 +0900

    chore : 케이크 주문 상태 enum 적용

---
+++ src/main/kotlin/com/example/cakezip/domain/cake/CakeStatusType.kt
@@ -0,0 +1,17 @@
+package com.example.cakezip.domain.cake
+
+import com.fasterxml.jackson.annotation.JsonFormat
+
+@JsonFormat(shape = JsonFormat.Shape.OBJECT)
+enum class CakeStatusType(
+    val cakeStatusName: String
+) {
+    CART("장바구니"), // 장바구니
+    PAYMENT("결제완료"), // 결제 완료
+    CANCEL("취소"), // 주문 취소
+    REJECT("거절"), // 주문 거절
+    PROCEED("준비중"), // 준비 중
+    READY("픽업대기중"), // 픽업 대기
+    COMPLETE("픽업완료"), // 픽업 완료
+    REVIEW("리뷰작성완료"), // 리뷰 작성 완료
+}
