b9285194273dae48661fe99f5109067bae2fa83a
hyerimmy <heyrim2010@naver.com>
2022-08-19 11:45:19 +0900

    chore : DTO 충돌 문제 해

--- src/main/kotlin/com/example/cakezip/dto/UserPaymentDto.kt
+++ src/main/kotlin/com/example/cakezip/dto/UserPaymentDto.kt
@@ -1,8 +1,6 @@
 package com.example.cakezip.dto
 
-import org.hibernate.internal.util.type.PrimitiveWrapperHelper.ShortDescriptor
-
-data class UserDto (
+data class UserPaymentDto (
     val name: String,
     val email: String,
     val phoneNum: String,
