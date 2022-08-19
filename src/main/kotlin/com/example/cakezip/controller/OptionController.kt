079480a90383d4d621215878f1964340edbfad03
hwihwi99 <chamgrace@naver.com>
2022-08-19 10:09:25 +0900

    feat : 옵션 추가 및 수정 진행 중

--- src/main/kotlin/com/example/cakezip/controller/OptionController.kt
+++ src/main/kotlin/com/example/cakezip/controller/OptionController.kt
@@ -3,18 +3,17 @@ package com.example.cakezip.controller
 import com.example.cakezip.domain.cake.Cake
 import com.example.cakezip.domain.cake.CakeOptionList
 import com.example.cakezip.domain.cake.OptionTitleType.*
+import com.example.cakezip.dto.NewOptionReqDto
 import com.example.cakezip.repository.CakeOptionListRepository
 import com.example.cakezip.service.OptionDetailService
+import com.example.cakezip.service.ShopService
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
-import org.springframework.web.bind.annotation.DeleteMapping
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.*
 import java.util.Optional
 
 @Controller
-class OptionController (private val optionDetailService: OptionDetailService, private val optionListRepository: CakeOptionListRepository){
+class OptionController (private val optionDetailService: OptionDetailService, private val shopService: ShopService,private val optionListRepository: CakeOptionListRepository){
 
     @GetMapping("/options/{type}/{shopId}")
     fun getShopDesignOption(@PathVariable("type") type:String,@PathVariable("shopId") shopId:Long, model: Model) : String{
@@ -27,9 +26,26 @@ class OptionController (private val optionDetailService: OptionDetailService, pr
             "creamcolor" -> optionDetailList = optionDetailService.getOptionDetailByShopAndType(shopId, CCOLOR)
             "letter" -> optionDetailList = optionDetailService.getOptionDetailByShopAndType(shopId, LCOLOR)
         }
+        model.addAttribute("shopId", shopId)
         model.addAttribute("type", type)
         model.addAttribute("optionDetail",optionDetailList)
         return "optionpage"
+    }
+
+    @GetMapping("/options/new/{type}/{shopId}")
+    fun addOptionPage(@PathVariable("shopId") shopId: Long, @PathVariable("type") type:String ,model: Model) : String {
+        model.addAttribute("shopId", shopId)
+        model.addAttribute("type", type)
+        model.addAttribute("form", NewOptionReqDto())
+        return "addOption"
+    }
+
+    @RequestMapping(value = arrayOf("/options/new/{type}/{shopId}"), method = arrayOf(RequestMethod.POST))
+    fun addOption(@PathVariable("shopId") shopId: Long, @PathVariable("type") type:String , model: Model) : String {
+        val shop = shopService.getByShopId(shopId)
+        model.addAttribute("shopId", shop.shopId)
+        model.addAttribute("form", NewOptionReqDto())
+        return "addOption"
     }
 
     @DeleteMapping("/options/delete/detail/{optionId}")
