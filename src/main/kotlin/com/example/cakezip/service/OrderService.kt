b7e9e96b140ff2dc0dc3e0c999cad37d8bf53d63
가영 <kykim097@gmail.com>
2022-08-21 23:14:48 +0900

    fix : 주문 조회, 주문 취소 기능 수정

--- src/main/kotlin/com/example/cakezip/service/OrderService.kt
+++ src/main/kotlin/com/example/cakezip/service/OrderService.kt
@@ -1,32 +1,19 @@
 package com.example.cakezip.service
 
 import com.example.cakezip.dto.OrderDto
-
 import com.example.cakezip.domain.Orders
-import com.example.cakezip.domain.Review
 import com.example.cakezip.domain.cake.Cake
 import com.example.cakezip.domain.member.Customer
-import com.example.cakezip.domain.shop.Shop
-import com.example.cakezip.repository.CakeRepository
-import com.example.cakezip.repository.CustomerRepository
-import com.example.cakezip.repository.OrderRepository
-import org.springframework.data.jpa.repository.Query
-import org.springframework.data.repository.query.Param
 import org.springframework.stereotype.Service
-import java.util.*
-import javax.persistence.Column
-import javax.persistence.JoinColumn
-import javax.persistence.ManyToOne
-import javax.persistence.OneToOne
 
 @Service
 interface OrderService {
-    fun getCustomerAllOrders(customerId: Long) : List<OrderDto>?
+    fun getCustomerAllOrders(customer: Customer) : List<OrderDto>?
 
     fun getCustomerOrders(cakeId: Long) : OrderDto
 
     fun changeCakeStateCancel(id: Long)
 
     fun addOrder(merchantUid: String, merchantPrice: Long, customer: Customer,cake: Cake): Orders
-    
 }
+
--- src/main/kotlin/com/example/cakezip/service/OrderServiceImpl.kt
+++ src/main/kotlin/com/example/cakezip/service/OrderServiceImpl.kt
@@ -8,35 +8,21 @@ import com.example.cakezip.domain.notice.NotificationMessage
 import com.example.cakezip.domain.notice.NotificationType
 import com.example.cakezip.dto.OrderDto
 import com.example.cakezip.repository.*
-import org.springframework.context.ApplicationEventPublisher
 import org.springframework.stereotype.Service
 
 @Service
 class OrderServiceImpl(
     private val orderRepository: OrderRepository,
     private val cakeRepository: CakeRepository,
-    private val customerRepository: CustomerRepository,
     private val noticeService: NotificationService,
 ) : OrderService {
-
-    // 오더 아이디로 케이크 아이디 찾기 -> 케이크 아이디로 띄워주기
-    //// customerId 받아와서 모든 오더 + 케이크 띄우기
-    //
-    override fun getCustomerAllOrders(CustomerId: Long): List<OrderDto>? {
-        val customer = customerRepository.findByCustomerId(CustomerId)
+    override fun getCustomerAllOrders(customer: Customer): List<OrderDto>? {
         val cakeList: ArrayList<OrderDto> = ArrayList()
 
         for (cake in cakeRepository.findByCustomerAndCakeStatusNot(customer, CakeStatusType.CART)) {
-            println("1")
             val order = orderRepository.findOrdersByCake(cake)
-            println("2")
             cakeList.add(OrderDto(order?.orderId, cake.customer.user.userName, cake.shop.shopName, cake))
-            println("3")
         }
-        println(cakeList)
-        println("4")
-        println("된다고?")
-
         return cakeList
     }
 
@@ -51,17 +37,17 @@ class OrderServiceImpl(
     override fun changeCakeStateCancel(CakeId: Long) {
         val cake = cakeRepository.findByCakeId(CakeId)
         val order = orderRepository.findOrdersByCake(cake)
-        //val statusCancel = CakeStatusType.CANCEL
 
         if (cake.cakeStatus.equals(CakeStatusType.CANCEL)) {
             //TODO: 이미 주문 취소일 경우 예외 처리
             //TODO: 상태가 준비중 전일 때만 주문 취소 가능하게 처리
+
             println("이미 CANCEL")
         } else {
             println("CANCEL 완")
             cake.cakeStatus = CakeStatusType.CANCEL
             cakeRepository.save(cake)
-            noticeService.makeNotice(
+            noticeService.makeNotification(
                 cake.customer.customerId, cake.shop.seller!!.sellerId!!, order,
                 NotificationMessage.ORDER_CANCEL, NotificationType.TOSELLER
             )
@@ -81,5 +67,5 @@ class OrderServiceImpl(
         return orderRepository.save(order)
     }
 
-
 }
+
