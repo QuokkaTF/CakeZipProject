4ae39aad607c57a5008a871c04f7a41330d06b86
hyerimmy <heyrim2010@naver.com>
2022-08-22 02:42:40 +0900

    chore : 검색 왕최종 / 키워드값 표시 추가

--- src/main/kotlin/com/example/cakezip/controller/SearchController.kt
+++ src/main/kotlin/com/example/cakezip/controller/SearchController.kt
@@ -1,23 +1,23 @@
 package com.example.cakezip.controller
 
 import com.example.cakezip.domain.member.Customer
+import com.example.cakezip.domain.member.Seller
 import com.example.cakezip.service.*
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.*
+import javax.servlet.http.HttpSession
 
 @Controller
 class SearchController(
     private val customerService: CustomerService,
     private val shopService: ShopService,
 ) {
-    //TODO : 경민한테 받으면 수정
-    var customer: Customer = customerService.findByCustomerId(2)
-
     @GetMapping("/search")
     fun getCartPaymentList(model: Model, keyword:String): String {
         var searchList = shopService.searchShop(keyword)
         model.addAttribute("searchList", searchList)
+        model.addAttribute("keyword", keyword)
         return "search"
     }
 }
