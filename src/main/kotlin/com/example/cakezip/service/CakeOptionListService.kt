0f3fbb80920446ff5d9ee74e419f406796d8c852
hyerimmy <heyrim2010@naver.com>
2022-08-22 06:03:38 +0900

    chore : 알고리즘 변경에 따라 사용하지 않는 함수 제거

--- src/main/kotlin/com/example/cakezip/service/CakeOptionListService.kt
+++ src/main/kotlin/com/example/cakezip/service/CakeOptionListService.kt
@@ -1,6 +1,7 @@
 package com.example.cakezip.service
 
 import com.example.cakezip.domain.cake.CakeOptionList
+import com.example.cakezip.domain.cake.OptionTitleType
 import com.example.cakezip.repository.CakeOptionListRepository
 import org.springframework.stereotype.Service
 import java.util.Optional
@@ -12,7 +13,4 @@ class CakeOptionListService(
     ) {
 
     fun findByCakeOptionListId(id: Long) : Optional<CakeOptionList> = cakeOptionListRepository.findByCakeOptionListId(id)
-
-    fun findByOptionTitleAndOptionDetail(title:String, detail:String):CakeOptionList =
-        cakeOptionListRepository.findByOptionTitleAndOptionDetail(title, detail)
 }
