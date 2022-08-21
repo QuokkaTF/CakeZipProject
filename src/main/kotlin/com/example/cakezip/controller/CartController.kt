6c94dffb957e0e49b57673013b2b8f6ad834058b
hyerimmy <heyrim2010@naver.com>
2022-08-22 06:06:31 +0900

    fix : 장바구니 추가 코드 왕완료, 장바구니조회, 결제화면조회 일부수정

--- src/main/kotlin/com/example/cakezip/controller/CartController.kt
+++ src/main/kotlin/com/example/cakezip/controller/CartController.kt
@@ -1,19 +1,12 @@
 package com.example.cakezip.controller
 
-import com.example.cakezip.domain.cake.CakeOptionList
 import com.example.cakezip.domain.cake.CakeStatusType
 import com.example.cakezip.domain.member.Customer
-import com.example.cakezip.domain.shop.Shop
 import com.example.cakezip.dto.UserPaymentDto
-
 import com.example.cakezip.service.*
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.*
-
-import java.util.*
-import kotlin.collections.ArrayList
-import kotlin.collections.HashMap
 
 @Controller
 class CartController(
@@ -22,50 +15,16 @@ class CartController(
     private val cakeTaskService: CakeTaskService,
     private val cakeOptionListService: CakeOptionListService,
     private val orderService: OrderService,
+    private val shopService: ShopService,
 ) {
-
-
     //TODO : 경민한테 받으면 수정
     var customer: Customer = customerService.findByCustomerId(2)
 
     @GetMapping("/users/cart")
     fun getCartList(model: Model): String {
-
-        var cake: ArrayList<HashMap<String, Any>> = ArrayList<HashMap<String, Any>>()
-        for (c in cakeService.findByCustomerAndCakeStatus(customer, CakeStatusType.CART)) {
-            var totalPrice: Long = 0
-            var cake_hashMap = HashMap<String, Any>()
-            for (ct in cakeTaskService.findByCake(c)) {
-                if (ct.cakeOptionList.cakeOptionListId != null) {
-                    var cakeOptionList: Optional<CakeOptionList> =
-                        cakeOptionListService.findByCakeOptionListId(ct.cakeOptionList.cakeOptionListId!!)
-                    cake_hashMap.put(cakeOptionList.get().optionTitle.toString(), cakeOptionList.get().optionDetail)
-                    cake_hashMap.put(
-                        cakeOptionList.get().optionTitle.toString() + "price",
-                        cakeOptionList.get().optionPrice
-                    )
-
-                    totalPrice += cakeOptionList.get().optionPrice
-                }
-            }
-            c.cakeId?.let { cake_hashMap.put("id", it) }
-            cake_hashMap.put("price", totalPrice)
-            cake_hashMap.put("shop", c.shop.shopName)
-            cake_hashMap.put("pickupdate", c.pickupDate)
-            cake_hashMap.put("letterText", c.letterText)
-            cake_hashMap.put("etc", c.etc)
-            cake.add(cake_hashMap)
-        }
-        println(cake)
+        var cake =
+            cakeService.getCakeOptionListAll(cakeService.findByCustomerAndCakeStatus(customer, CakeStatusType.CART))
         model.addAttribute("cake", cake)
-
-        // 결제 userDTO
-        val userinfo: UserPaymentDto = UserPaymentDto(customer.user.userName, customer.user.userEmail, customer.user.phoneNum)
-        model.addAttribute("userinfo", userinfo)
-        model.addAttribute("username", customer.user.userName)
-        model.addAttribute("userid", customer.user.userId)
-        model.addAttribute("useremail", customer.user.userEmail)
-
         return "cart"
     }
 
@@ -91,44 +50,12 @@ class CartController(
 
     @GetMapping("/users/cart/{cakeId}")
     fun getCartPaymentList(model: Model, @PathVariable cakeId: Long): String {
-
-        var cake: ArrayList<HashMap<String, Any>> = ArrayList<HashMap<String, Any>>()
         val c = cakeService.findByCakeId(cakeId)
-        var totalPrice: Long = 0
-        var cake_hashMap = HashMap<String, Any>()
-        for (ct in cakeTaskService.findByCake(c)) {
-            if (ct.cakeOptionList.cakeOptionListId != null) {
-                var cakeOptionList: Optional<CakeOptionList> =
-                    cakeOptionListService.findByCakeOptionListId(ct.cakeOptionList.cakeOptionListId!!)
-                cake_hashMap.put(cakeOptionList.get().optionTitle.toString(), cakeOptionList.get().optionDetail)
-                cake_hashMap.put(
-                    cakeOptionList.get().optionTitle.toString() + "price",
-                    cakeOptionList.get().optionPrice
-                )
-
-                totalPrice += cakeOptionList.get().optionPrice
-            }
-        }
-        c.cakeId?.let { cake_hashMap.put("id", it) }
-        cake_hashMap.put("price", totalPrice)
-        cake_hashMap.put("shop", c.shop.shopName)
-        cake_hashMap.put("pickupdate", c.pickupDate)
-        cake_hashMap.put("letterText", c.letterText)
-        cake_hashMap.put("etc", c.etc)
-        cake.add(cake_hashMap)
-
-        println(cake)
-        model.addAttribute("cake", cake)
-
-        // 결제 userDTO
-        val userinfo: UserPaymentDto = UserPaymentDto(customer.user.userName, customer.user.userEmail, customer.user.phoneNum)
+        model.addAttribute("cake", cakeService.getCakeOptionList(c))
+        val userinfo: UserPaymentDto =
+            UserPaymentDto(customer.user.userName, customer.user.userEmail, customer.user.phoneNum)
         model.addAttribute("userinfo", userinfo)
-        model.addAttribute("username", customer.user.userName)
-        model.addAttribute("userid", customer.user.userId)
-        model.addAttribute("useremail", customer.user.userEmail)
-
         return "payment"
-
     }
 
     @PostMapping("/users/cart/{cakeId}")
@@ -139,36 +66,22 @@ class CartController(
         return "redirect:/users/cart"
     }
 
-    //product temp
-    @GetMapping("/product")
-    fun getProduct(model: Model): String {
-        return "product"
-    }
-
     @PostMapping("/users/cart")
-    fun addCart(designCheck:String, sizeCheck:String, sheetCheck:String,
-                creamCheck:String, creamcolorCheck:String, letterCheck:String,
-                shop: Shop
+    fun addCart(
+        designCheck: Long, sizeCheck: Long, sheetCheck: Long, creamCheck: Long, creamcolorCheck: Long, letterCheck: Long,
+        shopId: Long, letterText:String, etc:String, date:String, time:String
     ): String {
-        cakeService.addCartCake("temp", "temp", "temp",
-        0, CakeStatusType.CARTTEMP, shop, customer)
-
-        val cake = cakeService.findByCustomerAndCakeStatus(customer, CakeStatusType.CARTTEMP)
-
-        val optionToAdd : ArrayList<CakeOptionList> = ArrayList<CakeOptionList>()
-        optionToAdd.add(cakeOptionListService.findByOptionTitleAndOptionDetail("DESIGN",designCheck))
-        optionToAdd.add(cakeOptionListService.findByOptionTitleAndOptionDetail("SIZE",sizeCheck))
-        optionToAdd.add( cakeOptionListService.findByOptionTitleAndOptionDetail("SFLAVOR",sheetCheck))
-        optionToAdd.add(cakeOptionListService.findByOptionTitleAndOptionDetail("CFLAVOR",creamCheck))
-        optionToAdd.add(cakeOptionListService.findByOptionTitleAndOptionDetail("CCOLOR",creamcolorCheck))
-        optionToAdd.add(cakeOptionListService.findByOptionTitleAndOptionDetail("LCOLOR",letterCheck))
-
-        for(option in optionToAdd){
-            cakeTaskService.addCartCakeTask(cake[0],option)
+        cakeService.addCartCake(
+            date+" "+time, letterText, etc,
+            0, CakeStatusType.CARTTEMP, shopService.getByShopId(shopId), customer
+        )
+        val cake = cakeService.findByCustomerAndCakeStatus(customer, CakeStatusType.CARTTEMP)[0]
+        val check: LongArray = longArrayOf(designCheck,sizeCheck,sheetCheck,creamCheck,creamcolorCheck,letterCheck)
+        for (c in check){
+            cakeTaskService.addCartCakeTask(cake, cakeOptionListService.findByCakeOptionListId(c).get())
         }
-
-        cakeService.updateCakeStatus(cake[0].cakeId!!, CakeStatusType.CART)
+        cakeService.sumPrice(cake)
+        cakeService.updateCakeStatus(cake.cakeId!!, CakeStatusType.CART)
         return "redirect:/users/cart"
     }
-
 }
