2b9f5e573c737350470c5056d1c6dd4853569ee1
hyerimmy <heyrim2010@naver.com>
2022-08-21 14:44:20 +0900

    chore : 리뷰 작성 별 프론트 수정, cakestatus에 따른 예외처리

--- src/main/kotlin/com/example/cakezip/controller/ReviewController.kt
+++ src/main/kotlin/com/example/cakezip/controller/ReviewController.kt
@@ -1,15 +1,13 @@
 package com.example.cakezip.controller
 
-import com.example.cakezip.domain.cake.CakeOptionList
-import com.example.cakezip.domain.cake.CakeTask
+import com.example.cakezip.domain.cake.Cake
+import com.example.cakezip.domain.cake.CakeStatusType
 import com.example.cakezip.domain.member.Customer
-import com.example.cakezip.repository.CustomerRepository
+import com.example.cakezip.dto.Message
 import com.example.cakezip.service.*
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.*
-import java.util.*
-import kotlin.collections.HashMap
 
 @Controller
 class ReviewController(
@@ -18,13 +16,11 @@ class ReviewController(
     private val cakeService: CakeService,
     private val cakeTaskService: CakeTaskService,
     private val cakeOptionListService: CakeOptionListService,
-
-    ) {
-
+) {
 
     //TODO : 경민한테 받으면 수정
     var customer: Customer = customerService.findByCustomerId(2)
-    
+
     @GetMapping("/reviews/customer/{customerId}")
     fun getMyReviews(model: Model, @PathVariable("customerId") customerId: Long): String {
         model.addAttribute("review", reviewService.getCustomerAllReviews(customerId))
@@ -41,37 +37,17 @@ class ReviewController(
 
     @GetMapping("/reviews/{cakeId}")
     fun directAddProduct(@PathVariable cakeId: Long, model: Model): String {
-        //TODO:해당 케이크에 대한 정보 출력할 수있도록 보내는거 (장바구니 코드 활용)
-        var cake: ArrayList<HashMap<String, Any>> = ArrayList<HashMap<String, Any>>()
-        val c = cakeService.findByCakeId(cakeId)
-        var totalPrice: Long = 0
-        var cake_hashMap = HashMap<String, Any>()
-        for (ct in cakeTaskService.findByCake(c)) {
-            if (ct.cakeOptionList.cakeOptionListId != null) {
-                var cakeOptionList: Optional<CakeOptionList> =
-                    cakeOptionListService.findByCakeOptionListId(ct.cakeOptionList.cakeOptionListId!!)
-                cake_hashMap.put(cakeOptionList.get().optionTitle.toString(), cakeOptionList.get().optionDetail)
-                cake_hashMap.put(
-                    cakeOptionList.get().optionTitle.toString() + "price",
-                    cakeOptionList.get().optionPrice
-                )
-
-                totalPrice += cakeOptionList.get().optionPrice
+        val c: Cake = cakeService.findByCakeId(cakeId)
+        model.addAttribute("cake", cakeService.getCakeOptionList(c))
+        if (cakeService.findByCakeId(cakeId).cakeStatus != CakeStatusType.COMPLETE) {
+            if (cakeService.findByCakeId(cakeId).cakeStatus == CakeStatusType.REVIEW) {
+                model.addAttribute("data", Message("이미 작성된 리뷰입니다.", "/"))
+            } else {
+                model.addAttribute("data", Message("리뷰를 작성할 수 없습니다.", "/"))
             }
+        } else {
+            model.addAttribute("data", Message("", ""))
         }
-        println("_________---_____^^*_______")
-        c.cakeId?.let { cake_hashMap.put("id", it) }
-        cake_hashMap.put("price", totalPrice)
-        cake_hashMap.put("shop", c.shop.shopName)
-        cake_hashMap.put("pickupdate", c.pickupDate)
-        cake_hashMap.put("letterText", c.letterText)
-        cake_hashMap.put("etc", c.etc)
-        cake.add(cake_hashMap)
-
-        println("=====================================")
-        println(cake)
-        model.addAttribute("cake", cake)
-
         return "review"
     }
 
@@ -80,15 +56,10 @@ class ReviewController(
         @PathVariable cakeId: Long, model: Model,
         reviewTitle: String, reviewContent: String, reviewScore: Int
     ): String {
-
-
         val cake = cakeService.findByCakeId(cakeId)
-
         model.addAttribute("cakeId", cakeId)
         reviewService.addReview(reviewTitle, reviewContent, reviewScore, cake)
-
+        cakeService.updateCakeStatus(cake.cakeId!!, CakeStatusType.REVIEW)
         return "redirect:/"
     }
-
 }
-
