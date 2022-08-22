f60dac8e3b6a025cbb63f40a13c4182d0f467218
hyerimmy <heyrim2010@naver.com>
2022-08-22 19:37:24 +0900

    feat : 리뷰 작성 세션 연결 완료, 예외처리 추가

--- src/main/kotlin/com/example/cakezip/controller/ReviewController.kt
+++ src/main/kotlin/com/example/cakezip/controller/ReviewController.kt
@@ -3,11 +3,15 @@ package com.example.cakezip.controller
 import com.example.cakezip.domain.cake.Cake
 import com.example.cakezip.domain.cake.CakeStatusType
 import com.example.cakezip.domain.member.Customer
+import com.example.cakezip.domain.member.User
+import com.example.cakezip.domain.member.UserType
 import com.example.cakezip.dto.Message
 import com.example.cakezip.service.*
+import org.springframework.dao.EmptyResultDataAccessException
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.*
+import javax.servlet.http.HttpSession
 
 @Controller
 class ReviewController(
@@ -18,8 +22,7 @@ class ReviewController(
     private val cakeOptionListService: CakeOptionListService,
 ) {
 
-    //TODO : 경민한테 받으면 수정
-    var customer: Customer = customerService.findByCustomerId(2)
+    val noAccessMessage: Message = Message("접근할 수 없는 페이지입니다.", "/")
 
     @GetMapping("/reviews/customer/{customerId}")
     fun getMyReviews(model: Model, @PathVariable("customerId") customerId: Long): String {
@@ -36,18 +39,28 @@ class ReviewController(
     }
 
     @GetMapping("/reviews/{cakeId}")
-    fun directAddProduct(@PathVariable cakeId: Long, model: Model): String {
-        val c: Cake = cakeService.findByCakeId(cakeId)
-        model.addAttribute("cake", cakeService.getCakeOptionList(c))
-        if (cakeService.findByCakeId(cakeId).cakeStatus != CakeStatusType.COMPLETE) {
-            if (cakeService.findByCakeId(cakeId).cakeStatus == CakeStatusType.REVIEW) {
-                model.addAttribute("data", Message("이미 작성된 리뷰입니다.", "/"))
+    fun directAddProduct(@PathVariable cakeId: Long, model: Model, session: HttpSession): String {
+        model.addAttribute("cake", cakeService.getCakeOptionList(cakeService.findByCakeId(cakeId)))
+        val user: User = session.getAttribute("user") as User
+        if (user.userType == UserType.CUSTOMER) {
+            val customer: Customer = session.getAttribute("customer") as Customer
+            if (cakeService.findByCakeId(cakeId).customer.customerId == customer.customerId) {
+                if (cakeService.findByCakeId(cakeId).cakeStatus != CakeStatusType.COMPLETE) {
+                    if (cakeService.findByCakeId(cakeId).cakeStatus == CakeStatusType.REVIEW) {
+                        model.addAttribute("data", Message("이미 작성된 리뷰입니다.", "/"))
+                    } else {
+                        model.addAttribute("data", Message("픽업 완료한 케이크만 리뷰를 작성할 수 있습니다.", "/"))
+                    }
+                } else {
+                    model.addAttribute("data", Message("", ""))
+                }
             } else {
-                model.addAttribute("data", Message("리뷰를 작성할 수 없습니다.", "/"))
+                model.addAttribute("data", noAccessMessage)
             }
         } else {
-            model.addAttribute("data", Message("", ""))
+            model.addAttribute("data", noAccessMessage)
         }
+
         return "review"
     }
 
