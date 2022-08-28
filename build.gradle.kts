d0765a30ae4c2c58e7b535f6e18ecee0f346c58a
benny1020 <benny1020@naver.com>
2022-08-28 16:08:17 +0900

    fix: build gradle redis 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,6 +25,10 @@ repositories {
 }
 
 dependencies {
+    implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-redis")
+    implementation("org.springframework.session:spring-session-data-redis")
     implementation("io.jsonwebtoken:jjwt:0.9.1")
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-web")
