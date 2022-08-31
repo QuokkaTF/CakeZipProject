4fb8f0a8dd1cb176f5b830ba5a7c085a253b3b61
benny1020 <benny1020@naver.com>
2022-08-31 11:11:43 +0900

    fix: 좋아요 개수 장바구니 개수 세션 반영

--- src/main/kotlin/com/example/cakezip/controller/MainController.kt
+++ src/main/kotlin/com/example/cakezip/controller/MainController.kt
@@ -6,10 +6,7 @@ import com.example.cakezip.domain.member.User
 import com.example.cakezip.domain.member.UserType
 import com.example.cakezip.domain.shop.Shop
 import com.example.cakezip.dto.Message
-import com.example.cakezip.service.LikeListService
-import com.example.cakezip.service.NotificationService
-import com.example.cakezip.service.ShopService
-import com.example.cakezip.service.UserService
+import com.example.cakezip.service.*
 import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
@@ -23,13 +20,13 @@ class MainController(
     private val notificationService: NotificationService,
     private val likeListService: LikeListService,
     private val userService: UserService,
+    private val cakeService: CakeService,
     ) {
     val noAccessMessage: Message = Message("접근할 수 없는 페이지입니다.", "/")
 
     @GetMapping("/home")
     fun getMainView(model: Model, session: HttpSession, @AuthenticationPrincipal user: User?): String {
         if(user != null) {
-
             val tmp = session.getAttribute("user") as User?
             if(session.getAttribute("user") as User? == null) {
                 session.setAttribute("user",user)
@@ -44,6 +41,7 @@ class MainController(
                 val customer = session.getAttribute("customer") as Customer
                 model.addAttribute("notification", notificationService.getCNotifications(customer))
                 session.setAttribute("likeCount",likeListService.getCustomerLikeCount(customer))
+                session.setAttribute("cartCount",cakeService.countCart(customer))
             } else {
                 val seller = session.getAttribute("seller") as Seller
                 model.addAttribute("notification", notificationService.getSNotifications(seller))
@@ -52,7 +50,6 @@ class MainController(
         }else {
             session.invalidate()
         }
-
 
         return "index"
     }
